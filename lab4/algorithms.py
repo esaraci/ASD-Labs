2657342ad56fe3f0bd1c6e40178e211b48989dcc
Eugen Saraci <eugen.saraci@gmail.com>
2018-05-07 14:02:57 +0200

    risolto thread held_karp

---
+++ lab4/algorithms.py
@@ -0,0 +1,158 @@
+from enum import Enum
+from PriorityQueue import PriorityQueue
+from graph import *
+import random
+import numpy as np
+
+# constants
+INFINITY = float('inf')
+
+
+class Color(Enum):
+    WHITE = 0
+    GREY = 1
+    BLACK = 2
+
+
+do_i_need_to_stop = False
+
+# _sons_of returns the sons of node v in the tree T
+# used by DFS
+def _sons_of(T, v):
+    return [x[0] for x in T if x[1] == v]
+
+
+# Basic DFS algorithm
+def DFS_Visited(T, u, visited, color):
+    color[u] = Color.BLACK
+    visited.append(u)
+    for v in _sons_of(T, u):
+        # don't need to check if u==v since u is set to black, and I check that v is white before proceeding
+        if color[v] == Color.WHITE:
+            visited = DFS_Visited(T, v, visited, color)
+
+    return visited
+
+
+# Get cycle cost, used by PRIM and Random Insertion
+# Input: Graph, C:[node1, node2, node3]
+# Output: cost of the CYCLE node1 --> node2 --> node3 --> node1
+def _get_cycle_cost(G, C):
+    adj_matrix = G.get_adj_matrix()
+    cost = 0
+    length_of_cycle = len(C)
+    for i in range(length_of_cycle):
+        j = i + 1
+        if j == length_of_cycle:
+            # if im out of bounds with j (=i+1)
+            # i'm finished, i set j=0 and add to the total cost the cost of the last edge
+            # v1 --> v2 --> v3 +[-->] v1
+            j = 0
+        cost += adj_matrix[C[i]][C[j]]
+
+    return cost
+
+
+# PRIM
+def prim(G, r=0):
+    keys = {}
+    parents = {}
+    adj_matrix = G.get_adj_matrix()
+
+    for i in range(G.get_len()):
+        keys[i] = INFINITY
+        parents[i] = None
+
+    keys[r] = 0
+
+    array = [[keys[v], v] for v in range(G.get_len())]
+    Q = PriorityQueue(array)
+
+    while not Q.is_empty():
+        u = Q.extract_min()  # u = array di 2 valori [chiave, nodo]
+
+        for v in range(G.get_len()):  # per ogni nodo nella lista delle adiacenze di u
+            # se u è in Q e il valore del nodo è minore di quello della chiave corrente e non è un cappio (elemento
+            # della diagonale)
+            if Q.is_there_element(v) and adj_matrix[u[1]][v] < keys[v] and u != v:
+                old_val = keys[v]  # salvo il vecchio valore (serve per decrease_key)
+                keys[v] = adj_matrix[u[1]][v]  # aggiorno il nuovo valore
+                parents[v] = u[1]  # modifico il genitore del nodo
+                Q.decrease_key(v, old_val, keys[v])  # decrem valore nell'albero per poi estrarre il minimo al prox giro
+
+    return get_prim_result(G, [(v, parents[v]) for v in range(G.get_len()) if v != r])
+
+
+# PRIM returns a tree, performing DFS on int will give me a cycle
+# after getting the cycle i can compute the cost of it using get_cycle_cost()
+def get_prim_result(G, tree):
+    color = {}
+    for i in tree:
+        color[i[0]] = Color.WHITE
+    # creating cycle from tree
+    C = DFS_Visited(tree, 0, [], color)
+
+    return _get_cycle_cost(G, C)
+
+
+# RANDOM INSERTION
+def random_insertion(G):
+
+    # BEGIN INITIALIZATION
+    C = [0]  # putting 0 as the first node since we always start from 0
+    adj_matrix = G.get_adj_matrix()
+    not_extracted = set((np.arange(G.get_len())))
+    not_extracted.remove(0)
+
+    min_ = INFINITY
+    node_idx = None
+    for i, val in enumerate(adj_matrix[0, 1:]):
+        if val < min_:
+            min_ = val
+            node_idx = i
+
+    # adding the second node
+    C.append(node_idx)
+    not_extracted.remove(node_idx)
+
+    # adding 3rd node, i do not need to perform any evaluation since i can attach it in only one way to C
+    # C = existing cycle with 2 nodes added before
+    extracted = random.sample(not_extracted, 1)[0]  # random.sample returns a list of length 1
+    C.append(extracted)
+    not_extracted.remove(extracted)
+    # END INITIALIZATION
+
+    # BEGIN SELECTION
+    while len(not_extracted) is not 0:
+        extracted = random.sample(not_extracted, 1)[0]
+        not_extracted.remove(extracted)
+
+        min_ = INFINITY
+        edges = None
+        for i, node in enumerate(C):
+            j = i + 1
+            if j == len(C):
+                # if i'm out of bounds with j (=i+1)
+                # I put j=0 (<-- first node of the cycle)
+                # "i" will the last node of the cycle
+                j = 0
+
+            # computing where (inside C) i need to insert "extracted" to minimize the increase of the cost
+            if adj_matrix[i][extracted] + adj_matrix[j][extracted] - adj_matrix[i][j] < min_:
+                min_ = adj_matrix[i][extracted] + adj_matrix[j][extracted] - adj_matrix[i][j]
+                edges = (i, j)
+
+        if edges[1] == 0:
+            # j == 0, so i need to insert this node as the last node of the cycle, linked with the 0 (= the first node)
+            # BEFORE: 0 --> v_1 ... --> v_n (--> 0)
+            # AFTER:  0 --> v_1 ... --> v_n --> "extracted" (--> 0)
+            C.append(extracted)
+        else:
+            # i need to insert "extracted" where j was.
+            # BEFORE: i --> j
+            # AFTER:  i --> extracted --> j
+            C.insert(edges[1],  extracted)
+
+    return _get_cycle_cost(G, C)
+
+
--- lab4/es1.py
+++ lab4/es1.py
@@ -1,148 +1,75 @@
 import numpy as np
 from graph import *
-from PriorityQueue import PriorityQueue
-import random
-from enum import Enum
 import threading
-from held_karp_thread import HeldKarpThread
+import time
+from algorithms import *
+from multiprocessing.pool import ThreadPool
+import signal
+import sys
 
 # global
 PI = 3.141592
 RRR = 6378.388
+stopper = False
+
 INFINITY = float('inf')
-# x[0][lat][long]
 
+do_i_need_to_stop = False
 
-class Color(Enum):
-    WHITE = 0
-    GREY = 1
-    BLACK = 2
+def held_karp(graph, v, S):
+    """
+    :param graph: Graph
+    :param v: target node
+    :param S: nodi poer cui dobbiamo passare
+    :return:
 
-
-def sons_of(T, v):
-    return [x[0] for x in T if x[1] == v]
-
-
-def DFS_Visited(T, u, visited, color):
-    color[u] = Color.BLACK
-    visited.append(u)
-    # adj_matrix = G.get_adj_matrix
-    for v in sons_of(T, u):
-        # non controllo con u diverso da v perchè discrimino usando il colore
-        if color[v] == Color.WHITE:
-            visited = DFS_Visited(T, v, visited, color)
-
-    return visited
-
-
-def prim(G, r=0):
-
-    keys = {}
-    parents = {}
-    adj_matrix = G.get_adj_matrix()
-
-    for i in range(G.get_len()):
-        keys[i] = INFINITY
-        parents[i] = None
-
-    keys[r] = 0
-
-    array = [[keys[v], v] for v in range(G.get_len())]
-    Q = PriorityQueue(array)
-
-    while not Q.is_empty():
-        u = Q.extract_min()  # u = array di 2 valori [chiave, nodo]
-
-        # print("MINIMO", u)
-
-        # print("ADJ", adj_matrix[u[1]])
-
-        for v in range(G.get_len()):  # per ogni nodo nella lista delle adiacenze di u
-            # print("Confronto", adj_matrix[u[1]][v])
-            # print("Valore corrente", keys[v])
-            # se u è in Q e il valore del nodo è minore di quello della chiave corrente e non è un cappio (elemento
-            # della diagonale)
-            if Q.is_there_element(v) and adj_matrix[u[1]][v] < keys[v] and u != v:
-                old_val = keys[v]  # salvo il vecchio valore (serve per decrease_key)
-                keys[v] = adj_matrix[u[1]][v]  # aggiorno il nuovo valore
-                parents[v] = u[1]  # modifico il genitore del nodo
-                Q.decrease_key(v, old_val, keys[v])  # decrem valore nell'albero per poi estrarre il minimo al prox giro
-
-    return [(v, parents[v]) for v in range(G.get_len()) if v != r]
-
-
-def get_cycle_cost(G, C):
-    adj_matrix = G.get_adj_matrix()
-    cost = 0
-    lungh = len(C)
-    for i in range(lungh):
-        j = i + 1
-        if j == lungh:
-            # se sono out of bound con j, torno a 0 e chiudo il ciclo
-            j = 0
-        cost += adj_matrix[C[i]][C[j]]
-
-    return cost
-
-
-def random_insertion(G):
-
-    # BEGIN INITIALIZATION
-    C = [0]
-    adj_matrix = G.get_adj_matrix()
-    not_extracted = set((np.arange(G.get_len())))
-    not_extracted.remove(0)
-
-    min_ = INFINITY
-    node_idx = None
-    for i, val in enumerate(adj_matrix[0, 1:]):
-        if val < min_:
-            min_ = val
-            node_idx = i
-
-    C.append(node_idx)
-    not_extracted.remove(node_idx)
-
-    # Estraggo terzo nodo a caso, esistono solo 2 possibili archi che lo integrano nel circuito
-    extracted = random.sample(not_extracted, 1)[0]  # random.sample ritorna una lista, prendo il primo ed unico elemento
-    C.append(extracted)
-    not_extracted.remove(extracted)
-
-    # END INITIALIZATION
-
-    # BEGIN SELECTION
-    while len(not_extracted) is not 0:
-        extracted = random.sample(not_extracted, 1)[0]
-        not_extracted.remove(extracted)
-
-        min_ = INFINITY
-        edges = None
-        for i, node in enumerate(C):
-            j = i + 1
-            if j == len(C):
-                # se sono out of bound con j, torno a 0 e chiudo il ciclo
-                j = 0
-
-            if adj_matrix[j][extracted] + adj_matrix[i][extracted] - adj_matrix[i][j] < min_:
-                min_ = adj_matrix[j][extracted] + adj_matrix[i][extracted] - adj_matrix[i][j]
-                edges = (i, j)
-
-        # collego l'ultimo nodo al primo
-        if edges[1] == 0:
-            C.append(extracted)
+    """
+    global do_i_need_to_stop
+    if do_i_need_to_stop is False:
+        # Caso base
+        if len(S) == 1:
+            # print("Caso base:", v)
+            return graph.get_adj_matrix(v, 0)  # S contiene un unico elemento che è v, stiamo andando da 0 —> v
+        elif graph.get_distances((v, S)) is not None:
+            return graph.get_distances((v, S))
         else:
-            C.insert(edges[1],  extracted)
+            min_dist = INFINITY
+            min_prec = None
+            S1 = tuple([u for u in S if u != v])
+            for u in S1:
+                dist = held_karp(graph, u, S1)  # chiamata ricorsiva
+                # print("Dist:", dist)
+                # print("Arco attuale:", graph.get_adj_matrix(u, v))
+                # print("Confronto:", dist + graph.get_adj_matrix(u, v), mindist)
+                if dist + graph.get_adj_matrix(u, v) < min_dist:  # aggiorno nel caso sia una quantità minore
+                    min_dist = dist + graph.get_adj_matrix(u, v)
+                    min_prec = u
+            graph.set_distances((v, S), min_dist)
+            graph.set_parents((v, S), min_prec)
+            return min_dist
+    else:
+        return INFINITY
 
-    return get_cycle_cost(G, C)
+
+def log_result(result):
+    print("HELD_KARP:", result)
+
+
+def waiter(sec):
+    time.sleep(sec)
+    global do_i_need_to_stop
+    do_i_need_to_stop = True
+    print("STOPPING THREAD")
 
 
 if __name__ == "__main__":
-    # print("min(len(lul))")
 
+    # BEGIN PREPROCESSING
+    # removing comments and checking coordinate type
     geo = False
     c = 1
-    fname = "datasets/pcb442.tsp"
-    with open(fname) as f:
+    f_name = "datasets/burma14.tsp"
+    with open(f_name) as f:
         line = f.readline()
         while line != "NODE_COORD_SECTION\n":
 
@@ -153,27 +80,29 @@ if __name__ == "__main__":
             line = f.readline()
             c += 1
 
-    dataset = np.loadtxt(fname, skiprows=c, comments=["EOF"])
+    dataset = np.loadtxt(f_name, skiprows=c, comments=["EOF"])
 
     l = len(dataset)
     lul = tuple(np.arange(l))
+    # END PREPROCESSING
 
+    # GENERATING GRAPH
     graph = Graph(dataset, geo)
 
-    T = prim(graph)
-    color = {}
-    for i in T:
-        color[i[0]] = Color.WHITE
-    D = DFS_Visited(T, 0, [], color)
+    pool = ThreadPool(processes=1)
+    result = pool.apply_async(held_karp, (graph, 0, lul), callback=log_result)
 
-    print("PRIM:", get_cycle_cost(graph, D))
-    # print("HELD_KARP:", held_karp(graph, 0, lul))
+    t = threading.Thread(target=waiter, args=(60*2, ))
+    t.daemon = True
+    t.start()
+    result.get()
+
+    # print("PRIM:", prim(graph))
     # print("RANDOM:", random_insertion(graph))
 
-    # griffo = HeldKarpThread(graph, 0, lul)
-    # griffo.start()
 
-    # x = threading.Thread(target=held_karp, args=(graph, 0, lul)).start()
+
+
 
 
 
